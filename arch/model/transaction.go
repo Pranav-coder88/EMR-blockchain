042ded763c1185b41abf9117ac736fa8536d2535
Vivek Radhakrishnan <vivekrk2000@gmail.com>
2023-05-07 21:54:56 +0530

    Added Functionality to form floating transaction from file

--- arch/model/transaction.go
+++ arch/model/transaction.go
@@ -12,6 +12,10 @@ func (transaction *Transaction) Read(filepath string) {
 	transaction.Record = *JSONToEMR(filepath)
 }
 
+func TransactionFromFile(filepath string) *Transaction {
+	return &Transaction{ Record: *JSONToEMR(filepath) }
+}
+
 func (transaction *Transaction) Writable() string {
 	var onPaper string
 	json.Unmarshal(transaction.Record.Bytable(), &onPaper)
