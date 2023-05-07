c777dcdda724ffd03e78b423a445e4d0676b16a3
Vivek Radhakrishnan <vivekrk2000@gmail.com>
2023-05-07 22:47:56 +0530

    Test case for blockchain data consistency

--- arch/main.go
+++ arch/main.go
@@ -5,14 +5,22 @@ import (
 	"model"
 )
 
+func printChain(bc *model.Blockchain) {
+	for index, block := range bc.Chain.Links() {
+		fmt.Println("\nindex:", index)
+		fmt.Printf("Hashcode: %x\n", block.Hash)
+		fmt.Println("block created on", block.Timestamp)
+		fmt.Println("record created on", block.Transaction.Record.PersonalData.CreatedDate)
+	}
+}
+
 func main() {
 	bc := model.NewBlockchain(2)
+	wallet := model.NewWallet()
 
 	bc.AddTransactions([]model.Transaction{ *model.TransactionFromFile("model/dummy.json") })
 	
-	if bc.IsChainValid() {
-		fmt.Println("valid") 
-	} else {
-		fmt.Println("invalid")
-	}
+	bc.ProcessPendingTransactions(wallet.GetWalletID())
+
+	printChain(bc)
 }
